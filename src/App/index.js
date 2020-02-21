eadb4fd45dc089544145e526709c754f35bc8012
Gabriel Nasicmento <gabriell_ca@outlook.com>
2020-02-21 17:13:18 -0300

    bootstrap

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        INITIAL
+        <Button variant="success">Bootstrap</Button>
       </header>
     </div>
   );
